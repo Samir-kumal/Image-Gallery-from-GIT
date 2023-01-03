53989c943fd66c8cee6bd1a138410719f88383a6
Samir-kumal <kumalsameer124@gmail.com>
2023-01-03 17:26:08 +0545

    updated

--- Imagegallerywithreact/src/App.jsx
+++ Imagegallerywithreact/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
     <>
     <h1>{Images.length}</h1>
       <Navbar/> 
-      <div className='grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 ml-2 mr-2'>
+      <div className='grid grid-cols-1 gap-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ml-2 mr-2'>
        {Images.map((image)=>(
         <Image key={image.id} {...image}/>
        ))}
