c78dffda6e1ff2226d46d14956973f00a6031c17
Samir-Kumal <kumalsameer124@gmail.com>
2023-05-10 15:48:14 +0545

    Changes reverted

--- reactImageGallery/src/components/Content.jsx
+++ reactImageGallery/src/components/Content.jsx
@@ -42,7 +42,7 @@ const content = (props) => {
                 href={imgContent}
                 download={true}
               >
-                Download
+                Download Image
               </a>
             </div>
           </div>
