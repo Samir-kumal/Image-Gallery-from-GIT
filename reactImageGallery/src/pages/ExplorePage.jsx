0ea1313af7e3e63814c29556925843a3dccc5870
Samir-kumal <kumalsameer124@gmail.com>
2024-01-28 22:22:00 +0545

    style: change the background color && refactor: add data array and map function

--- reactImageGallery/src/pages/ExplorePage.jsx
+++ reactImageGallery/src/pages/ExplorePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ExploreContainerLayout from '../components/Layout/ExploreContainer/ExploreContainerLayout'
+import Navbar from '../components/Navbar'
 
 const ExplorePage = () => {
   return (
     <>
+    <Navbar/>
       <ExploreContainerLayout/>
     </>
   )
